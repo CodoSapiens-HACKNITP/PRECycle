05125115795af81978ebf5e38cc1c1f710e91dd4
Digvijay Srivastava <dig9074vijay@gmail.com>
2021-01-17 03:22:56 +0530

    Added photos to about us

--- client/src/components/AboutUs/AboutComponent.js
+++ client/src/components/AboutUs/AboutComponent.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,15 +6,48 @@ import {
   CardBody,
   CardHeader,
   Jumbotron,
+  Media,
+  Row,
+  Col,
 } from "reactstrap";
+import Image from "react-bootstrap/Image";
 import { Link } from "react-router-dom";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
+function Team(props) { 
+  return(<Media>
+    <Row>
+      <Col md={4}>
+        <Image
+          src={props.src}
+          style={{ maxWidth: 125, maxHeight: 125}}
+          alt="Precycle"
+          roundedCircle
+          fluid
+        />
+      </Col>
+      <Col md={{ offset: 1, size: 7 }}>
+        <Media body className="ml-1">
+          <Media heading>
+            <h2 style={{ fontFamily: "Joti one", color: "black" }}>
+              {props.name}
+            </h2>
+          </Media>
+          <p>
+            {props.post}
+          </p>
+        </Media>
+      </Col>
+    </Row>
+  </Media>);
+}
 
 function About(props) {
   useEffect(() => {
-    AOS.init({duration:2000});
-  },[])
+    AOS.init({ duration: 2000 });
+  }, []);
   return (
     <div className="container">
       <div className="row">
@@ -46,9 +79,20 @@ function About(props) {
               </div>
             </div>
           </Jumbotron>
-         
+
           <h2>About</h2>
-          <p>PRECycle started on 15th January 2021 as a web development project based on Sustainable Development for HACKNITP Winter '21. We aim to create greater awareness about recycling. Making people recognise types of waste and the importance of processing them is a small step in that direction. We believe that this initiative will inspire more people to opt for recycling waste instead of getting inappropriately rid of them. The concept of sustainability is something we need to incorporate into our daily lives. It is quintessential in ensuring the wellness of future generations and, we at PRECycle are all about it!</p>
+          <p>
+            PRECycle started on 15th January 2021 as a web development project
+            based on Sustainable Development for HACKNITP Winter '21. We aim to
+            create greater awareness about recycling. Making people recognise
+            types of waste and the importance of processing them is a small step
+            in that direction. We believe that this initiative will inspire more
+            people to opt for recycling waste instead of getting inappropriately
+            rid of them. The concept of sustainability is something we need to
+            incorporate into our daily lives. It is quintessential in ensuring
+            the wellness of future generations and, we at PRECycle are all about
+            it!
+          </p>
           <p></p>
         </div>
         <div className="col-12 col-md-5">
@@ -71,50 +115,42 @@ function About(props) {
           </Card>
         </div>
         <div className="col-12">
-        <div data-aos="fade-up">
-          <Card>
-            <CardBody className="bg-faded">
-              <blockquote className="blockquote">
-                <p className="mb-0">
-                  Sustainable development is the pathway to the future we want
-                  for all. It offers a framework to generate economic growth,
-                  achieve social justice, exercise environmental stewardship and
-                  strengthen governance.
-                </p>
-                <footer className="blockquote-footer">
-                  Ban-Ki-Moon
-                  <cite title="Source Title"> ,General Secratary, UN 2014</cite>
-                </footer>
-              </blockquote>
-            </CardBody>
-          </Card>
+          <div data-aos="fade-up">
+            <Card>
+              <CardBody className="bg-faded">
+                <blockquote className="blockquote">
+                  <p className="mb-0">
+                    Sustainable development is the pathway to the future we want
+                    for all. It offers a framework to generate economic growth,
+                    achieve social justice, exercise environmental stewardship
+                    and strengthen governance.
+                  </p>
+                  <footer className="blockquote-footer">
+                    Ban-Ki-Moon
+                    <cite title="Source Title">
+                      {" "}
+                      ,General Secratary, UN 2014
+                    </cite>
+                  </footer>
+                </blockquote>
+              </CardBody>
+            </Card>
           </div>
         </div>
       </div>
-      <div className="row row-content">
-      <div data-aos="fade-up">
-        <div className="col-12">
-          <h2>Meet our team</h2>
-          <ul className="list-unstyled">
-            <br />
-            <li>
-              <b>Fuhar Suman</b>, <i>Captain</i>
-            </li>
-            <hr />
-            <li>
-              Palak Kumari, <i>Chief Managing Director</i>
-            </li>
-            <hr />
-            <li>
-              Rishabh Mishra, <i>Chief Operating Officer</i>
-            </li>
-            <hr />
-            <li>
-              Digvijay Srivastava, <i>Chief Technology Officer</i>
-            </li>
-          </ul>
-        </div>
-        </div>
+      
+        <div data-aos="fade-up">
+          
+            <h2>Meet our team</h2>
+            <br/><br/>
+          
+          
+              <Team src="./assets/fuhar.jpeg" name="Fuhar Suman" post="Team Captain" />
+              <Team src="./assets/palak.jpeg" name="Palak Kumari" post="Chief Managing Director" />
+              <Team src="./assets/rishabh.jpeg" name="Rishabh Mishra" post="Chief Operating Officer" />
+              <Team src="./assets/dig.png" name="Digvijay Srivastava" post="OP" />
+           
+          
       </div>
     </div>
   );
