d0d1bcf2e5c75e727589496741b8ef604084e45f
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-01-20 03:28:01 +0530

    Heroku deploy

--- server.js
+++ server.js
@@ -22,6 +22,15 @@ app.use("/seller", require("./routes/seller"));
 app.use("/rider", require("./routes/rider"));
 app.use("/vendor", require("./routes/vendor"));
 
+
+//Heroku deploy code
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get("*", (req, res)=> {
+    res.sendFile(path.resolve(__dirname,'client','build', 'index.html'));
+  })
+}
 //run the server
 const port = process.env.port || 1210;
 app.listen(port, () =>
