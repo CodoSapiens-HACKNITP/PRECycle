37136dbf1526caf5ff2e24a7818b2a2541009a2c
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-02-01 10:11:02 +0530

    db uri removed

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoURI");
+const db = process.env.mongoURI || config.get("mongoURI");
 
 const connectDB = async () => {
   try {
